0c16eaadea18cdd64986803ab0c036e1a078789a
0TickPulse <0tickpulse@gmail.com>
2024-01-04 15:48:24 +0800

    update netty dependency again

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ repositories {
 dependencies {
     implementation("org.spigotmc:spigot-api:${project.properties["craftbukkit.version"]}")
     implementation("com.denizenscript:denizen:${project.properties["denizen.version"]}")
-    implementation("io.netty:netty-all:4.1.48.Final")
+    implementation("io.netty:netty-all:4.1.86.Final")
     implementation("me.clip:placeholderapi:2.10.0")
     implementation("com.ticxo.modelengine:ModelEngine:${project.properties["modelengine.version"]}")
 }
