c0b3ad293e2821a456584fd9f647f7ec12868f14
hyper <honkhonk4456@gmail.com>
2024-04-24 16:16:21 -0400

    Some changes to that PR

--- src/main/java/net/tickmc/megizen/bukkit/events/MegBaseEntityInteractScriptEvent.java
+++ src/main/java/net/tickmc/megizen/bukkit/events/MegBaseEntityInteractScriptEvent.java
@@ -16,28 +16,27 @@ public class MegBaseEntityInteractScriptEvent extends BukkitScriptEvent implemen
 
     // <--[event]
     // @Events
-    // meg player interact with model
+    // meg player interacts with model
     //
     // @Group Player
     //
     // @Location true
     //
-    // @Warning this event may in some cases double-fire, requiring usage of the 'ratelimit' command (like 'ratelimit <player> 1t') to prevent doubling actions.
+    // @Warning This event may in some cases double-fire, requiring usage of the 'ratelimit' command (like 'ratelimit <player> 1t') to prevent doubling firing.
     //
-    // @Triggers when a player interacts with Modelengine active model.
+    // @Triggers when a player interacts with ModelEngine model.
     //
     // @Context
-    // <context.active_model> returns the MegActiveModelTag of interacted entity.
-    // <context.modeled_entity> returns the MegModeledEntityTag of interacted entity.
-    // <context.click_position> returns a LocationTag of the click position (as a world-less vector, relative to the entity's center).
-    // <context.action> returns a cause of the interaction. Can be: ATTACK, INTERACT or INTERACT_ON.
+    // <context.active_model> returns the MegActiveModelTag of the entity.
+    // <context.click_position> returns a LocationTag of the click position (as a world-less vector, relative to the entity's center), if any.
+    // <context.action> returns interaction cause. Can be: ATTACK, INTERACT or INTERACT_ON.
     //
     // @Player Always.
     //
     // -->
 
     public MegBaseEntityInteractScriptEvent() {
-        registerCouldMatcher("meg player interact with model");
+        registerCouldMatcher("meg player interacts with model");
     }
 
     public BaseEntityInteractEvent event;
@@ -59,7 +58,6 @@ public class MegBaseEntityInteractScriptEvent extends BukkitScriptEvent implemen
     public ObjectTag getContext(String name) {
         return switch (name) {
             case "active_model" -> new MegActiveModelTag(event.getModel());
-            case "modeled_entity" -> new MegModeledEntityTag(event.getModel().getModeledEntity());
             case "click_position" -> event.getClickedPosition() != null ? new LocationTag(event.getClickedPosition()) : null;
             case "action" -> new ElementTag(event.getAction().name(), true);
             default -> super.getContext(name);
