cd178e7ce876db3dca37a07a66457e27d171d2a1
hyper <39167664+heypr@users.noreply.github.com>
2025-03-01 18:44:38 -0500

    Removed unused import

--- src/main/java/net/tickmc/megizen/bukkit/events/PlayerInputScriptEvent.java
+++ src/main/java/net/tickmc/megizen/bukkit/events/PlayerInputScriptEvent.java
@@ -10,8 +10,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInputEvent;
 
-import java.util.Arrays;
-
 public class PlayerInputScriptEvent extends BukkitScriptEvent implements Listener {
 
     // <--[event]
